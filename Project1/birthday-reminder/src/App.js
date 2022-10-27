d070d1bade1b702958f7c82eb7585e714774e9b4
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-10-27 20:29:14 +0530

    third commit

--- Project1/birthday-reminder/src/App.js
+++ Project1/birthday-reminder/src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import data from './data';
 import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(data)
+  function clear(){
+    setPeople([])
+    document.getElementById("btn").style.cursor="default"
+  }
   return (
-    <>
-    
-    </>
+    <div className='whole'>
+    <h3>{people.length} birthdays today</h3>
+    <List people={people}/>
+    <button id='btn' onClick={clear}>Clear contents</button>
+    </div>
   );
 }
 
--- Project1/birthday-reminder/src/List.js
+++ Project1/birthday-reminder/src/List.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './App.css'
 
-const List = () => {
+const List = ({people}) => {
   return (
-    <div>List Contents</div>
+    <>
+    <div className='list'>List Contents</div>
+    {people.map((person)=>{
+      const {id, name, age, image} = person;
+      return <article key={id}>
+        <img className='img' src={image} alt="" />
+        <div className='div1'>
+        <div className='name'>Name: {name}</div>
+        <div className='age'>Age: {age}</div>
+        </div>
+      </article>
+    })}
+    </>
   )
 }
 
