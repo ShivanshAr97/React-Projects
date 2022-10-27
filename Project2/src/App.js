17ef5e3cd1c195e64e0f4e44a1003dee524baecd
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-10-27 21:00:50 +0530

    fifth commit

---
+++ Project2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { useState , useEffect} from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  return (
+    <>
+    Tours Project Setup
+    </>
+  );
+}
+
+export default App;
---
+++ Project2/src/Tour.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+
+const Tour = () => {
+  return (
+    <div>Tour</div>
+  )
+}
+
+export default Tour
