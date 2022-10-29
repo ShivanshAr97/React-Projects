37f9ecadb4c793a7f29200d13037a94eadaf614c
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-10-29 18:24:31 +0530

    ninth commit

---
+++ Project4/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    Lorem ipsum dolor sit amet.
+    </>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This repository has the projects made in the 15 projects FreeCodeCamp video.
 - Birthday reminders
 - Tours
 - Reviews
+- Accordion
