0603a0d81fb8bc9290d90bf9ead369b1635ee76a
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-10-30 17:31:53 +0530

    fifteenth commit

---
+++ Project7/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import data from './data'
+
+function App() {
+  return (
+    <>
+    Lorem ipsum dolor sit amet.
+    </>
+  );
+}
+
+export default App;
