a61a39f92bb6b113a89a6f9959d361568ce9d6c9
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-11-05 22:57:46 +0530

    ninteenth commit

--- Project10/src/App.js
+++ Project10/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Alert from './Alert'
 import List from './List'
 
 function App() {
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({show:true, text:'', type:''})
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log("Clicked");
+  }
   return (
     <>
-    Lorem ipsum dolor sit amet.
+    <form onSubmit={handleSubmit}>
+    {alert.show && <Alert/>}
+    <input type="text" placeholder='Complete homework' value={name} onChange={(e)=>setName(e.target.value)} name="" id="" />
+    <button type='submit'>{isEditing?'Edit':'Submit'}</button>
+    </form>
+    <List/>
+    <button>Clear List</button>
     </>
   );
 }
